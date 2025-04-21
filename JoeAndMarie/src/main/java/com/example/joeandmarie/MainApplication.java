d343da27f8a8495f9dce7adf0ed93471636d642d
Ivann James Paradero <Ivannjames.paradero@cit.edu>
2025-04-21 23:24:45 +0800

    MainMenu and fonts

--- JoeAndMarie/src/main/java/com/example/joeandmarie/MainApplication.java
+++ JoeAndMarie/src/main/java/com/example/joeandmarie/MainApplication.java
@@ -1,36 +1,21 @@
 package com.example.joeandmarie;
 
-import com.almasb.fxgl.app.ApplicationMode;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.app.MenuItem;
 import com.almasb.fxgl.app.scene.FXGLMenu;
-import com.almasb.fxgl.app.scene.LoadingScene;
 import com.almasb.fxgl.app.scene.SceneFactory;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.physics.PhysicsComponent;
-import javafx.application.Platform;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressIndicator;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.awt.*;
 import java.util.EnumSet;
 
-import static com.almasb.fxgl.dsl.FXGLForKtKt.getApp;
-import static com.almasb.fxgl.dsl.FXGLForKtKt.set;
 
 public class MainApplication extends GameApplication {
     private Entity player1, player2;
