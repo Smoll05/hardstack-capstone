ec9d978ce0c6220894717bc0c40f96f7568aa910
choclitshake <bobownumber4@gmail.com>
2025-05-08 23:45:57 +0800

    layouts for other menu screens

--- JoeAndMarie/src/main/java/com/example/joeandmarie/Controller/CreditsController.java
+++ JoeAndMarie/src/main/java/com/example/joeandmarie/Controller/CreditsController.java
@@ -1,4 +1,33 @@
 package com.example.joeandmarie.Controller;
 
+import com.example.joeandmarie.MainApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class CreditsController {
+    @FXML private AnchorPane apContainer;
+
+    @FXML
+    private void handleExitClick() {
+        switchScreenToMainMenu();
+    }
+
+    private void switchScreenToMainMenu() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/assets/layouts/joe_main_menu.fxml"));
+            Parent newContent = fxmlLoader.load();
+
+            // Clear the current content and add the new content
+            apContainer.getChildren().clear();
+            apContainer.getChildren().add(newContent);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
