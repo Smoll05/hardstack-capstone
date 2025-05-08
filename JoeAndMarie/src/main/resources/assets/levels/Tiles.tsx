513bba0fb97220c37eaf968c56d5ac19de0239f1
Ivann James Paradero <Ivannjames.paradero@cit.edu>
2025-05-08 14:18:59 +0800

    images and maps

---
+++ JoeAndMarie/src/main/resources/assets/levels/Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Tiles" tilewidth="32" tileheight="32" tilecount="242" columns="11">
+ <image source="Tiles.png" width="360" height="720"/>
+</tileset>
